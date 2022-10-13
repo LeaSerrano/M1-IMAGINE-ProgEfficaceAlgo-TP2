dc6fd4d47a704345aea5410cd0119e2b88f95692
lea-serrano <leas19112001@yahoo.fr>
2022-10-13 18:14:52 +0200

    Avancée

--- eval-perf.cpp
+++ eval-perf.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <math.h>
 #include <cmath>
+#include <algorithm>
 
 #define C1 0.2f
 #define C2 0.3f
@@ -169,6 +170,8 @@ double & Ex10_reduce_mult_Qu1(double* V, size_t n) {
     return temp[0];
 }
 
+
+
 /*double & Ex10_reduce_mult_Qu2(int32_t V, size_t n) {
     __m256d x_vec, x_vec_V;
     int32_t* temp = new double[4];
@@ -203,7 +206,13 @@ double & Ex10_reduce_add_Qu1(double* V, size_t n) {
 
     x_vec_V = _mm256_set1_pd(0.0);
 
-    std::cout << V[0]+V[1]+V[2]+V[3]+V[4]+V[5] << std::endl;
+    int resAttendu = 0;
+
+    for (int elt = 0; elt < n; elt++) {
+        resAttendu += V[elt];
+    }
+
+    std::cout << "resultat attendu : " << resAttendu << std::endl;
 
     for (size_t i = 0; i < n-3; i+=4) {
         x_vec = _mm256_loadu_pd(V+i);
@@ -227,25 +236,32 @@ double & Ex10_reduce_add_Qu1(double* V, size_t n) {
 
 int32_t & Ex10_reduce_add_Qu2(int32_t* V, size_t n) {
     __m256i x_vec, x_vec_V;
-    int32_t* temp = new int32_t[4];
+    int32_t* temp = new int32_t[8];
 
     x_vec_V = _mm256_set1_epi32(0);
 
-    std::cout << V[0]+V[1]+V[2]+V[3]+V[4]+V[5] << std::endl;
+    int resAttendu = 0;
 
-    for (size_t i = 0; i < n-3; i+=4) {
-        x_vec = _mm256_loadu_si256((__m256 *)V+i);
-        x_vec_V = _mm256_add_epi32(x_vec_V, x_vec);
+    for (int elt = 0; elt < n; elt++) {
+        resAttendu += V[elt];
     }
 
-    _mm256_storeu_epi32(temp, x_vec_V);
+    std::cout << "resultat attendu : " << resAttendu << std::endl;
 
-    size_t mod = n -n %4;
+    for (size_t i = 0; i < n-7; i+=8) {
+        x_vec = _mm256_loadu_si256((__m256i *)(V+i));
+        x_vec_V = _mm256_add_epi32(x_vec_V, x_vec);
+    }
+    
+    _mm256_storeu_si256((__m256i *)temp, x_vec_V);
+
+    size_t mod = n - n % 8;
+
     for (size_t j = n-1; j >= mod; j--) {
         temp[0] += V[j];
     }
 
-    for(size_t i = 1; i < 4; i++) {
+    for(size_t i = 1; i < 8; i++) {
         temp[0] += temp[i];
     }
 
@@ -253,13 +269,94 @@ int32_t & Ex10_reduce_add_Qu2(int32_t* V, size_t n) {
 
 }
 
-int32_t reduce3(int32_t *tab, size_t n) {
-    int32_t sum = 1;
-    for (int i = 0; i < n; i++) {
-        sum *= tab[i];
+float Ex11_SIMD(float* V, size_t n) {
+    __m256 x_vec, x_vec_V;
+    float* temp = new float[8];
+
+    x_vec_V = _mm256_loadu_ps(V);
+
+    for (size_t i = 0; i < n-7; i+=8) {
+        x_vec = _mm256_loadu_ps(V+i);
+        x_vec_V = _mm256_min_ps(x_vec_V, x_vec);
     }
-    return sum;
+
+    _mm256_storeu_ps(temp, x_vec_V);
+
+    size_t mod = n - n % 8;
+
+    for (size_t j = n-1; j >= mod; j--) {
+        if (V[j] < temp[0]) {
+            temp[0] = V[j];
+        }
+    }
+
+    for(size_t i = 1; i < 8; i++) {
+        if (temp[i] < temp[0]) {
+            temp[0] = temp[i];
+        }
+    }
+
+    return temp[0];
 }
+
+float Ex11_Naif(float* V, size_t n) {
+    float min = V[0];
+    for (size_t i = 0; i < n; i++) {
+        if (V[i] < min) {
+            min = V[i];
+        }
+    }
+    return min;
+}
+
+float Ex11_Fonction(float* V, size_t n) {
+    return *std::min_element(V, V+n);
+
+}
+
+//Essai ex12
+double* Ex12(double* tab, size_t n, double alpha) {
+    __m256d a = _mm256_set1_pd(1);
+    __m256d registre, registreAlpha, registreAdd, resultat;
+    double* r = new double[4];
+    double* rResultat = new double[4];
+
+    for (size_t i = 0; i < n-3; i+=4) {
+        registre = _mm256_loadu_pd(tab);
+        registreAlpha = _mm256_add_pd(registre, registreAlpha);
+        registreAlpha = _mm256_mul_pd(registreAlpha, a);
+        a = _mm256_set1_pd(alpha*alpha*alpha*alpha);
+    }
+    _mm256_storeu_pd(r, registreAlpha);
+
+    for (size_t i = 0; i < n; i++) {
+        registreAdd = _mm256_add_pd(registreAdd, registreAlpha);
+    }
+
+    _mm256_storeu_pd(rResultat, registreAlpha);
+
+    int a2;
+    for (size_t i = 0; i < 4; i++) {
+        if (i == 0) {
+            a2 = 1;
+        }
+        else if (i == 1) {
+            a2 = alpha;
+        }
+        else if (i == 2) {
+            a2 = alpha*alpha;
+        }
+        else {
+            a2 = alpha*alpha*alpha;
+        }
+        
+        rResultat[i] = rResultat[i]*a2;
+
+    }
+
+    return rResultat;
+}
+
 
 int main() {
   int n, N;
@@ -370,9 +467,45 @@ int main() {
   V_Ex10[3] = 9;//4;
   V_Ex10[4] = 2;//5;
   V_Ex10[5] = 8;//6;
-  double res_Ex10_mult = 1.;
-  double res_Ex10_add = 0.;
   size_t n_Ex10 = 6;
+
+  int32_t* V_Ex10_2 = new int32_t[10];
+  V_Ex10_2[0] = 31;
+  V_Ex10_2[1] = 20;
+  V_Ex10_2[2] = 93;
+  V_Ex10_2[3] = 44;
+  V_Ex10_2[4] = 25;
+  V_Ex10_2[5] = 61;
+  V_Ex10_2[6] = 56;
+  V_Ex10_2[7] = 32;
+  V_Ex10_2[8] = 20;
+  V_Ex10_2[9] = 18;
+  size_t n_Ex10_2 = 10;
+
+  //Ex11
+  float* tab_Ex11 = new float[9];
+  tab_Ex11[0] = 3;
+  tab_Ex11[1] = 5;
+  tab_Ex11[2] = 1;
+  tab_Ex11[3] = 9;
+  tab_Ex11[4] = 2;
+  tab_Ex11[5] = 3;
+  tab_Ex11[6] = 4;
+  tab_Ex11[7] = 10;
+  tab_Ex11[8] = 6;
+  size_t n_Ex11 = 9;
+
+  //Ex12
+  double* tab_Ex12_1 = new double[8];
+  tab_Ex12_1[0] = 1;
+  tab_Ex12_1[1] = 1;
+  tab_Ex12_1[2] = 1;
+  tab_Ex12_1[3] = 1;
+  tab_Ex12_1[4] = 1;
+  tab_Ex12_1[5] = 1;
+  tab_Ex12_1[6] = 1;
+  tab_Ex12_1[7] = 1;
+  int x_Ex12_1 = 2;
 
 
   PE.start();
@@ -406,8 +539,16 @@ int main() {
   Ex9_Horner (resultat_Ex9, tab_Ex9, x_Ex9, indice_Ex9_2, sizeP_Ex9);*/
 
   //double resultat1 = Ex10_reduce_mult_Qu1(V_Ex10, n_Ex10);
-  double resultat2 = Ex10_reduce_add_Qu1(V_Ex10, n_Ex10);
-  std::cout << resultat2 << std::endl;
+  //double resultat2 = Ex10_reduce_add_Qu1(V_Ex10, n_Ex10);
+  //int32_t resultat3 = Ex10_reduce_add_Qu2(V_Ex10_2, n_Ex10_2);
+
+   /*float resultat4 = Ex11_SIMD(tab_Ex11, n_Ex11);
+   float resultat5 = Ex11_Naif(tab_Ex11, n_Ex11);
+   float resultat6 = Ex11_Fonction(tab_Ex11, n_Ex11);*/
+
+   /*double* resultat7 = Ex12(tab_Ex12_1,8, x_Ex12_1);
+
+  std::cout << "resultat obtenu : " << resultat7[0] << std::endl;*/
 
   //pour récup un pointeur &(v[0]) ou v.data()
 
